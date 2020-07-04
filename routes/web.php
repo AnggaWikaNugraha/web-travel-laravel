7dc3adc2edb787f9ef77db9cb0e1afc3cffb7535
AnggaWikaNugraha <37723902+AnggaWikaNugraha@users.noreply.github.com>
2020-07-04 21:34:57 +0700

    template admin dasboard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// prefix untuk akses url localhost/admin/
+// namespace untuk memanggil namespsenya
+
+Route::prefix('admin')
+->namespace('Admin')
+->group(function(){
+      Route::get('/' , 'DasboardController@index')
+      ->name('dasboard');
+});
