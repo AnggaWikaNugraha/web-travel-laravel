c306906701a8efcd9a86233f655611bcb846b9bf
AnggaWikaNugraha <37723902+AnggaWikaNugraha@users.noreply.github.com>
2020-07-05 17:52:11 +0700

    mulai lagi

--- resources/views/includes/user/script.blade.php
+++
@@ -1,4 +0,0 @@
-<script src="frontend/libraries/jquery-3.5.1.min.js "></script>
-<script src="frontend/libraries/bootstrap-4.5.0-dist/js/bootstrap.js "></script>
-<script type="text/javascript" src="frontend/scripts/main.js"></script>
-<script src="frontend/libraries/xzoom/xzoom.min.js"></script>
