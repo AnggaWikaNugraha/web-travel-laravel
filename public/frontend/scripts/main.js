782cbc692755829df79df5efc2e861474be17141
AnggaWikaNugraha <37723902+AnggaWikaNugraha@users.noreply.github.com>
2020-07-05 14:17:24 +0700

    home pgaes

---
+++ public/frontend/scripts/main.js
@@ -0,0 +1,12 @@
+AOS.init({
+    duration: 1000,
+});
+
+$(document).ready(function() {
+    $('.xzoom, .xzoom-gallery').xzoom({
+        zoomwidth: 500,
+        title: false,
+        tint: '#333',
+        Xoffset: 15
+    })
+})
---
+++ resources/views/includes/user/footer.blade.php
@@ -0,0 +1,47 @@
+<footer class="section-footer mt-5 mb-4 border-top">
+    <div class="container pt-5 pb-5">
+        <div class="row">
+            <div class="col-12">
+                <div class="row footer-wadah justify-content-between">
+                    <div class="col-lg3 ">
+                        <h5>FEATURES</h5>
+                        <ul class="list-unstyled ">
+                            <li><a href="# ">Revoius</a></li>
+                            <li><a href="# ">Community</a></li>
+                            <li><a href="# ">Social Media Kit</a></li>
+                            <li><a href="# ">Affiliate</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg3 ">
+                        <h5>FEATURES</h5>
+                        <ul class="list-unstyled ">
+                            <li><a href="# ">Revoius</a></li>
+                            <li><a href="# ">Community</a></li>
+                            <li><a href="# ">Social Media Kit</a></li>
+                            <li><a href="# ">Affiliate</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg3 ">
+                        <h5>FEATURES</h5>
+                        <ul class="list-unstyled ">
+                            <li><a href="# ">Revoius</a></li>
+                            <li><a href="# ">Community</a></li>
+                            <li><a href="# ">Social Media Kit</a></li>
+                            <li><a href="# ">Affiliate</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg3 ">
+                        <h5>FEATURES</h5>
+                        <ul class="list-unstyled ">
+                            <li><a href="# ">Revoius</a></li>
+                            <li><a href="# ">Community</a></li>
+                            <li><a href="# ">Social Media Kit</a></li>
+                            <li><a href="# ">Affiliate</a></li>
+                        </ul>
+                    </div>
+                    </h5>
+                </div>
+            </div>
+        </div>
+
+</footer>
