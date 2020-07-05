34a0025f24204c4eedb3bcf19601189bae77c232
AnggaWikaNugraha <37723902+AnggaWikaNugraha@users.noreply.github.com>
2020-07-05 15:12:01 +0700

    mindahin navbar ke includes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,68 +9,7 @@
 </head>
 
 <body>
-    <!-- navbar -->
-    <div class="container">
-        <div class="row navbar navbar-expand-lg navbar-light bg-transparent">
-
-            <!-- logo -->
-            <a class="navbar-brand" href="#">
-
-            </a>
-            <!-- akhir logo -->
-
-            <!-- bikin button collapse , ketika di hp dan tablet akan muncul -->
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navb">
-                  <span class="navbar-toggler-icon"></span>
-            </button>
-            <!-- akhir button collapes -->
-
-            <!-- menu navbar -->
-            <div class="collapse navbar-collapse" id="navb">
-                <ul class="navbar-nav ml-auto mr-3">
-                    <li class="nav-item mx-md-2">
-                        <a href="#" class="nav-link active">Home</a>
-                    </li>
-                    <li class="nav-item mx-md-2">
-                        <a href="#" class="nav-link">Paket Travel</a>
-                    </li>
-                    <li class="nav-item mx-md-2">
-                        <a href="#" class="nav-link">Promo</a>
-                    </li>
-
-                    <!-- dropdown -->
-                    <li class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" id="navbardrop" data-toggle="dropdown">Service</a>
-                        <div class="dropdown-menu">
-                            <a href="#" class="dropdown-item">Link-1</a>
-                            <a href="#" class="dropdown-item">Link-2</a>
-                            <a href="#" class="dropdown-item">Link-3</a>
-                        </div>
-                    </li>
-                    <!-- akhir dropdown -->
-
-                    <li class="nav-item mx-md-2">
-                        <a href="#" class="nav-link">Testimonial</a>
-                    </li>
-                </ul>
-
-                <!-- button mobile -->
-                <form class="form-inline d-sm-block d-md-none">
-                    <button class="btn btn-login my-2 my-sm-0">masuk</button>
-                </form>
-                <!-- akhir button mobile -->
-                <!-- button mdekstop -->
-                <form class="form-inline d-none d-md-block my-2 my-lg-0">
-                    <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4">MASUK</button>
-                </form>
-                <!-- akhir button dekstop -->
-
-            </div>
-            <!-- akhir menu navbar -->
-
-        </div>
-    </div>
-    <!-- akhir navbar -->
+    @include('includes.user.navbar')
     @yield('content')
 
     @include('includes.user.footer')
